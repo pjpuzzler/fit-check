a1c0e408aac54fda29956fada53bef51910f509d
pjpuzzler <pjpuzzler@gmail.com>
2021-06-10 10:43:25 -0400

    c

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { UserProvider } from "./src/context/UserContext";
+import { FirebaseProvider } from "./src/context/FirebaseContext";
+
+import AppStackScreens from "./src/stacks/AppStackScreens";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <FirebaseProvider>
+            <UserProvider>
+                <NavigationContainer>
+                    <AppStackScreens />
+                </NavigationContainer>
+                <StatusBar style="auto" />
+            </UserProvider>
+        </FirebaseProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
