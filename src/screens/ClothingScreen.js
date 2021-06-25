3a2e0dba49fc9848c4e7dadb79be66bb335db9c9
Patrick Johnson <31143551+pjpuzzler@users.noreply.github.com>
2021-06-25 10:21:38 -0400

    c

--- src/screens/ClothingScreen.js
+++ src/screens/ClothingScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Platform, StatusBar, Dimensions } from "react-native";
 import styled from "styled-components";
 import { MaterialCommunityIcons } from "react-native-vector-icons";
@@ -6,15 +6,28 @@ import { MaterialCommunityIcons } from "react-native-vector-icons";
 import { UserContext } from "../context/UserContext";
 
 import Text from "../components/Text";
+import clothes from "../../assets/clothingData"
 
 export default ClothingScreen = ({ route, navigation }) => {
     const [user, setUser] = useContext(UserContext);
-
-    const [wardrobeClothing, setWardrobeClothing] = useState(true);
-
+    
     const clothingType = route.params.clothingType;
     const isCloset = route.params.isCloset;
+    
+    const clothingData = clothes.filter(clothing => clothing.type === clothingType);
+
+    const [wardrobeClothing, setWardrobeClothing] = useState(true);
+    const [filter, setFilter] = useState("all");
+    const [data, setData] = useState(clothingData);
+
     const windowWidth = Dimensions.get("window").width;
+    
+    useEffect(() => {
+        if (filter !== "all")
+            setData(clothingData.filter(clothing => clothing.setting === filter));
+        else
+            setData(clothingData);
+    }, [filter]);
 
     return (
         <Container>
@@ -45,6 +58,34 @@ export default ClothingScreen = ({ route, navigation }) => {
                     />
                 </TO>
             </TopBar>
+
+            <FiltersContainer>
+                <TO onPress={() => setFilter("all")}>
+                    <MaterialCommunityIcons
+                        name="all-inclusive"
+                        size={windowWidth / 12}
+                        color={filter === "all" ? "#1c4068" : "#666666"}
+                    />
+                </TO>
+
+                <TO onPress={() => setFilter("casual")}>
+                    <MaterialCommunityIcons
+                        name="white-balance-sunny"
+                        size={windowWidth / 12}
+                        color={filter === "casual" ? "#1c4068" : "#666666"}
+                    />
+                </TO>
+
+                <TO onPress={() => setFilter("formal")}>
+                    <MaterialCommunityIcons
+                        name="account-tie"
+                        size={windowWidth / 12}
+                        color={filter === "formal" ? "#1c4068" : "#666666"}
+                    />
+                </TO>
+            </FiltersContainer>
+
+            <ClothingList data={data} renderItem={} initialNumToRender={9} keyExtractor={() => } numColumns={3} />
         </Container>
     );
 };
@@ -64,3 +105,15 @@ const TopBar = styled.SafeAreaView`
 `;
 
 const TO = styled.TouchableOpacity``;
+
+const FiltersContainer = styled.SafeAreaView`
+    width: 95%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ClothingList = styled.FlatList`
+    width: 100%;
+    height: 100%;
+`;
